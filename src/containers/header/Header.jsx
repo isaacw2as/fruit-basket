e37475b5de3685562573679a4b0682c697fe4ca3
isaacw2as <isaacyun03@gmail.com>
2022-10-22 20:10:06 -0700

    finished header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './header.css'
+import people from '../../assets/people.png';
+import circle from '../../assets/Ellipse 1.png';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='fruitbasket__header section__padding' id='home'>
+      <div className='fruitbasket__header-content'>
+        <h1 className='gradient__text'>Hello - Welcome to the Fruit Basket!</h1>
+        <p>NO BITCHES?NO BITCHES?NO BITCHES?NO BITCHES?NO BITCHES?NO BITCHES?NO BITCHES?NO BITCHES?NO BITCHES?NO BITCHES?NO BITCHES?</p>
+        <div className='fruitbasket__header-content__input'>
+          <input type='email' placeholder='Your Email Address'></input>
+          <button type='button'>Get Started</button>
+        </div>
+
+        <div className='fruitbasket__header-content__people'>
+          <img src={people} alt='people'/>
+          <p>...1600 bitches waiting for you...</p>
+        </div>
+      </div>
+      <div className='fruitbasket__header-image'>
+          <img src={circle} alt='circle'/>
+      </div>
     </div>
   )
 }
