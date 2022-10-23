4b19b027c1500a3299f3c513edb4d7535f86b9f2
isaacw2as <isaacyun03@gmail.com>
2022-10-22 17:59:02 -0700

    holy shit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
 import {Footer, Blog, Possibility, Features, WhatFruitBasket, Header} from './containers';
+import {CTA, Brand, Navbar} from './components';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatFruitBasket />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className= 'fruitbasket__navbar'>
+      <div className='fruitbasket__navbar-links'>
+        <div className='fruitbasket__navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+      </div>
     </div>
   )
 }
