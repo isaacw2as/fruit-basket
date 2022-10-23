f7f97339b6dd518755a0d081bb62f75256a53410
isaacw2as <isaacyun03@gmail.com>
2022-10-22 17:10:54 -0700

    created components

---
+++ src/containers/WhatFruitBasket/WhatFruitBasket.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './whatFruitBasket.css'
+
+const WhatFruitBasket = () => {
+  return (
+    <div>
+      WhatFruitBasket
+    </div>
+  )
+}
+
+export default WhatFruitBasket
